1067ee6f472489f9bd5ab93c8658e9cc80cd771d
IsakFe <isak.fehric@gmail.com>
2017-03-16 14:58:53 +0100

    Design Monthly Overview
    16.3.2017

---
+++ app/src/main/java/com/eeinternship/timetracker/MonthlyOverview.java
@@ -0,0 +1,24 @@
+package com.eeinternship.timetracker;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MonthlyOverview extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monthly_overview);
+
+        // status bar color
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.colorStatusBar));
+    }
+}
