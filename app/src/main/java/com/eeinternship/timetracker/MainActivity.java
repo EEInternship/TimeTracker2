829a78aa672b020dc1fd6ced78be35d3af2574b2
IsakFe <isak.fehric@gmail.com>
2017-03-17 14:41:46 +0100

    Start Work Design
    17.3.2017

--- app/src/main/java/com/eeinternship/timetracker/MainActivity.java
+++ app/src/main/java/com/eeinternship/timetracker/MainActivity.java
@@ -1,13 +1,18 @@
 package com.eeinternship.timetracker;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStartWork;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -20,6 +25,16 @@ public class MainActivity extends AppCompatActivity {
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(this.getResources().getColor(R.color.colorStatusBar));
+
+        // btnStartWork
+        btnStartWork=(Button)findViewById(R.id.button_start_work);
+        btnStartWork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nextActivity=new Intent(getApplication(),StartWorkActivity.class);
+                startActivity(nextActivity);
+            }
+        });
 
     }
 }
