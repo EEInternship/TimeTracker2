d74ee6fae70ec6c89fadad1d7775a8c5aa352fe5
isak.fehric@gmail.com <isak.fehric@gmail.com>
2017-04-07 09:18:22 +0200

    test 3

--- app/src/main/java/eeinternship/com/timetracker/MainActivity.java
+++ app/src/main/java/eeinternship/com/timetracker/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             userData.setUserAcount(accountName);
             applicationTimeTracker.setUserData(userData);
         }
+        ///
 
 
         ActionBar actionBar = getSupportActionBar();
