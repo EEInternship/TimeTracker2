493d9801b87638445dea8ef5bc5f54759f86212a
IsakFe <isak.fehric@gmail.com>
2017-04-21 14:11:05 +0200

    21.4.2017

--- app/src/main/java/eeinternship/com/timetracker/AlarmReciverPerTime.java
+++ app/src/main/java/eeinternship/com/timetracker/AlarmReciverPerTime.java
@@ -14,13 +14,12 @@ import android.support.v4.app.NotificationCompat;
 public class AlarmReciverPerTime extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         boolean isScreenOn = pm.isScreenOn();
-        if(!isScreenOn)
-        {
-            PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK |PowerManager.ACQUIRE_CAUSES_WAKEUP |PowerManager.ON_AFTER_RELEASE,"MyLock");
+        if (!isScreenOn) {
+            PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.ON_AFTER_RELEASE, "MyLock");
             wl.acquire(10000);
-            PowerManager.WakeLock wl_cpu = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"MyCpuLock");
+            PowerManager.WakeLock wl_cpu = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyCpuLock");
 
             wl_cpu.acquire(10000);
         }
@@ -28,13 +27,12 @@ public class AlarmReciverPerTime extends BroadcastReceiver {
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
                         .setSmallIcon(R.drawable.img_notification_logo)
-                        .setContentTitle("Tickets")
-                        .setLights(0xFFff0000,100,100)
-                        .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
-                        .setContentText("Are you still working on same ticket? Don't forget to create new tickets!");
+                        .setContentTitle("TimeTracker")
+                        .setLights(0xFFff0000, 100, 100)
+                        .setVibrate(new long[]{1000, 1000, 1000, 0, 0})
+                        .setContentText("Are you still working on same ticket?");
 
         NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(1, mBuilder.build());
-
     }
 }
